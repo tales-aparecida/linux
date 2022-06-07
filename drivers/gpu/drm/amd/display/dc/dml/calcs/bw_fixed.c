22f51a690cff85c9d26a5d9bd028b25bbdba7e45
Maíra Canal <maira.canal@usp.br>
2022-06-07 20:35:07 -0300

    drm/amd/display: Move bw_fixed macros to header file
    The macros defined at bw_fixed are important mathematical definitions,
    specifying masks to get the fractional part and the maximum and
    minimum values of I64. In order to enable unit tests for bw_fixed,
    it is relevant to have access to those macros. This commit moves the
    macros to the header file, making it accessible to future unit tests.
    
    Co-developed-by: Magali Lemes <magalilemes00@gmail.com>
    Signed-off-by: Magali Lemes <magalilemes00@gmail.com>
    Signed-off-by: Maíra Canal <maira.canal@usp.br>
    Co-developed-by: Tales Aparecida <tales.aparecida@gmail.com>
    Signed-off-by: Tales Aparecida <tales.aparecida@gmail.com>

--- drivers/gpu/drm/amd/display/dc/dml/calcs/bw_fixed.c
+++ drivers/gpu/drm/amd/display/dc/dml/calcs/bw_fixed.c
@@ -26,19 +26,7 @@
 #include "bw_fixed.h"
 
 
-#define MIN_I64 \
-	(int64_t)(-(1LL << 63))
-
-#define MAX_I64 \
-	(int64_t)((1ULL << 63) - 1)
-
-#define FRACTIONAL_PART_MASK \
-	((1ULL << BW_FIXED_BITS_PER_FRACTIONAL_PART) - 1)
-
-#define GET_FRACTIONAL_PART(x) \
-	(FRACTIONAL_PART_MASK & (x))
-
-static uint64_t abs_i64(int64_t arg)
+uint64_t abs_i64(int64_t arg)
 {
 	if (arg >= 0)
 		return (uint64_t)(arg);
