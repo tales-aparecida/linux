c8e586901f71716836fc23ea2e0ee3ce3ebbf499
David Gow <davidgow@google.com>
2022-06-18 17:03:10 +0800

    mmc: sdhci-of-aspeed: test: Use kunit_test_suite() macro
    The kunit_test_suite() macro is no-longer incompatible with module_add,
    so its use can be reinstated.
    
    Since this fixes parsing with builtins and kunit_tool, also enable the
    test by default when KUNIT_ALL_TESTS is enabled.
    
    The test can now be run via kunit_tool with:
    	./tools/testing/kunit/kunit.py run --arch=x86_64 \
    	--kconfig_add CONFIG_OF=y --kconfig_add CONFIG_OF_ADDRESS=y \
    	--kconfig_add CONFIG_MMC=y --kconfig_add CONFIG_MMC_SDHCI=y \
    	--kconfig_add CONFIG_MMC_SDHCI_PLTFM=y \
    	--kconfig_add CONFIG_MMC_SDHCI_OF_ASPEED=y \
    	'sdhci-of-aspeed'
    
    (It may be worth adding a .kunitconfig at some point, as there are
    enough dependencies to make that command scarily long.)
    
    Signed-off-by: David Gow <davidgow@google.com>

--- drivers/mmc/host/sdhci-of-aspeed.c
+++ drivers/mmc/host/sdhci-of-aspeed.c
@@ -606,25 +606,6 @@ static struct platform_driver aspeed_sdc_driver = {
 
 #if defined(CONFIG_MMC_SDHCI_OF_ASPEED_TEST)
 #include "sdhci-of-aspeed-test.c"
-
-static inline int aspeed_sdc_tests_init(void)
-{
-	return __kunit_test_suites_init(aspeed_sdc_test_suites);
-}
-
-static inline void aspeed_sdc_tests_exit(void)
-{
-	__kunit_test_suites_exit(aspeed_sdc_test_suites);
-}
-#else
-static inline int aspeed_sdc_tests_init(void)
-{
-	return 0;
-}
-
-static inline void aspeed_sdc_tests_exit(void)
-{
-}
 #endif
 
 static int __init aspeed_sdc_init(void)
@@ -639,12 +620,6 @@ static int __init aspeed_sdc_init(void)
 	if (rc < 0)
 		goto cleanup_sdhci;
 
-	rc = aspeed_sdc_tests_init();
-	if (rc < 0) {
-		platform_driver_unregister(&aspeed_sdc_driver);
-		goto cleanup_sdhci;
-	}
-
 	return 0;
 
 cleanup_sdhci:
@@ -656,8 +631,6 @@ module_init(aspeed_sdc_init);
 
 static void __exit aspeed_sdc_exit(void)
 {
-	aspeed_sdc_tests_exit();
-
 	platform_driver_unregister(&aspeed_sdc_driver);
 	platform_driver_unregister(&aspeed_sdhci_driver);
 }
