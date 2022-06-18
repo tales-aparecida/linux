988240c840a1cccd836a075757a4d17a151c191a
Daniel Latypov <dlatypov@google.com>
2022-06-18 17:03:07 +0800

    kunit: flatten kunit_suite*** to kunit_suite** in .kunit_test_suites
    We currently store kunit suites in the .kunit_test_suites ELF section as
    a `struct kunit_suite***` (modulo some `const`s).
    For every test file, we store a struct kunit_suite** NULL-terminated array.
    
    This adds quite a bit of complexity to the test filtering code in the
    executor.
    
    Instead, let's just make the .kunit_test_suites section contain a single
    giant array of struct kunit_suite pointers, which can then be directly
    manipulated.
    
    Signed-off-by: Daniel Latypov <dlatypov@google.com>
    Co-developed-by: David Gow <davidgow@google.com>
    Signed-off-by: David Gow <davidgow@google.com>

--- include/kunit/test.h
+++ include/kunit/test.h
@@ -303,9 +303,9 @@ size_t kunit_suite_num_test_cases(struct kunit_suite *suite);
 unsigned int kunit_test_case_num(struct kunit_suite *suite,
 				 struct kunit_case *test_case);
 
-int __kunit_test_suites_init(struct kunit_suite * const * const suites);
+int __kunit_test_suites_init(struct kunit_suite * const * const suites, int num_suites);
 
-void __kunit_test_suites_exit(struct kunit_suite **suites);
+void __kunit_test_suites_exit(struct kunit_suite **suites, int num_suites);
 
 #if IS_BUILTIN(CONFIG_KUNIT)
 int kunit_run_all_tests(void);
@@ -316,10 +316,10 @@ static inline int kunit_run_all_tests(void)
 }
 #endif /* IS_BUILTIN(CONFIG_KUNIT) */
 
-#define __kunit_test_suites(unique_array, unique_suites, ...)		       \
-	static struct kunit_suite *unique_array[] = { __VA_ARGS__, NULL };     \
-	static struct kunit_suite **unique_suites			       \
-	__used __section(".kunit_test_suites") = unique_array
+#define __kunit_test_suites(unique_array, ...)				       \
+	static struct kunit_suite *unique_array[]			       \
+	__aligned(sizeof(struct kunit_suite *))				       \
+	__used __section(".kunit_test_suites") = { __VA_ARGS__ }
 
 /**
  * kunit_test_suites() - used to register one or more &struct kunit_suite
@@ -337,7 +337,6 @@ static inline int kunit_run_all_tests(void)
  */
 #define kunit_test_suites(__suites...)						\
 	__kunit_test_suites(__UNIQUE_ID(array),				\
-			    __UNIQUE_ID(suites),			\
 			    ##__suites)
 
 #define kunit_test_suite(suite)	kunit_test_suites(&suite)
