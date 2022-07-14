be53a3921a19cf9ed03a948b89d26d24e9c992de
Felix Kuehling <Felix.Kuehling@amd.com>
2022-07-14 17:51:02 -0400

    drm/ttm: fix missing NULL check in ttm_device_swapout
    Backport of Christian's eponymous patch to amd-staging-drm-next. This
    branch may be nearly obsolete, but this patch may still be worth
    applying as it can serve as a template for backports to some release
    branches. It fixes intermittent kernel oopses when memory is severely
    overcommitted.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/ttm/ttm_device.c
+++ drivers/gpu/drm/ttm/ttm_device.c
@@ -157,6 +157,9 @@ int ttm_device_swapout(struct ttm_device *bdev, struct ttm_operation_ctx *ctx,
 			list_for_each_entry(bo, &man->lru[j], lru) {
 				uint32_t num_pages = PFN_UP(bo->base.size);
 
+				if (!bo->resource)
+					continue;
+
 				ret = ttm_bo_swapout(bo, ctx, gfp_flags);
 				/* ttm_bo_swapout has dropped the lru_lock */
 				if (!ret)
