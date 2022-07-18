92e7c41848d0e578bb5f4353fa41abcb49741e4a
Tales Aparecida <tales.aparecida@gmail.com>
2022-07-18 19:09:44 -0300

    drm/amd/dc: Alias std Linux limits on bw_fixed macros
    Fix two checkpatch errors regarding complex macros by replacing their
    definition with the standard Linux macros for integer limits.
    
    ERROR: Macros with complex values should be enclosed in parentheses
    +#define MIN_I64 \
    +       (int64_t)(-(1LL << 63))
    
    ERROR: Macros with complex values should be enclosed in parentheses
    +#define MAX_I64 \
    +       (int64_t)((1ULL << 63) - 1)
    
    Signed-off-by: Tales Aparecida <tales.aparecida@gmail.com>

--- drivers/gpu/drm/amd/display/dc/dml/calcs/bw_fixed.c
+++ drivers/gpu/drm/amd/display/dc/dml/calcs/bw_fixed.c
@@ -26,11 +26,9 @@
 #include "bw_fixed.h"
 
 
-#define MIN_I64 \
-	(int64_t)(-(1LL << 63))
+#define MIN_I64 S64_MIN
 
-#define MAX_I64 \
-	(int64_t)((1ULL << 63) - 1)
+#define MAX_I64 S64_MAX
 
 #define FRACTIONAL_PART_MASK \
 	((1ULL << BW_FIXED_BITS_PER_FRACTIONAL_PART) - 1)
